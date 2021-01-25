67251150bd9dc4d20fe95fc4d8869e6269bdffb4
dydtjr1128 <cys_star@naver.com>
2021-01-25 11:10:01 +0900

    스프링 클라우드 수정

--- msa-consumer/src/main/java/com/dydtjr1128/msaconsumer/client/ProductFeignClient.java
+++ msa-consumer/src/main/java/com/dydtjr1128/msaconsumer/client/ProductFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 URL 명시 시 > 순수하게 Feign Client로 동작@FeignClient(name = "${producer.server.name}", url="http://localhost:8081")
 URL 명시하지 않을 시 > Feign + Ribbon + Eureka 모드로 동작, 서버 호출은
  */
-@FeignClient(name = "${producer.server.name}")
+@FeignClient(name = "${producer.server.name}")//producer.server.name=producer-api
 public interface ProductFeignClient {
 
     @GetMapping(value = "/api/v1/product/name")
